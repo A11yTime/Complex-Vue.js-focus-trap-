441074fceee65b1edc02ff0f7c90ac9065e08852
aciosui <mrana.swe@gmail.com>
2024-04-16 18:55:26 -0400

    Vue Dialo

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <AddUser />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AddUser from './components/AddUser.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AddUser
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
+
+</style>./components/AddUser.1vue
